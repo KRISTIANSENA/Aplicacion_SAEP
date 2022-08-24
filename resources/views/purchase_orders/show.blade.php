ec3090f0faa90f70a4c6c01a2ca4b4608a5f87e9
KRISTIANSENA <90565251+KRISTIANSENA@users.noreply.github.com>
2022-08-23 20:35:45 -0500

    actualización de visitas
    corrección de errores

--- resources/views/purchase_orders/show.blade.php
+++ resources/views/purchase_orders/show.blade.php
@@ -3,29 +3,31 @@
 
 
     <div class="container">
-        <h5>DETALLES {{$product->product_name}} </h5>
+        <h5>Detalle ordenes de compra de {{$purchase_order->id}} </h5>
         <table>
             <thead>
-            <tr>
-                <th>Numero de id </th>
-                <th>Nombre del producto</th>
-                <th>Marca</th>
-                <th>Modelo</th>
 
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>{{ $product->id}}</td>
-                <td>{{$product->product_name}}</td>
-                <td>{{$product->brand}}</td>
-                <td>{{ $product->model}}</td>
-            </tr>
-            </tbody>
+        <tr><th>Numero de id  </th><td>{{ $purchase_order->id}}</td></tr>
+        <tr><th>Identificación de empleado</th> <td>{{$purchase_order->employees->employee_name}}</td></tr>
+        <tr><th>Identificación de proveedor</th> <td>{{ $purchase_order->providers->provider_name}}</td></tr>
+        <tr><th>Identificación del producto</th><td>{{ $purchase_order->products->product_name}}</td></tr>
+        <tr><th>Detalle</th><td>{{ $purchase_order->detail}}</td></tr>
+        <tr><th>Monto</th><td>{{ $purchase_order->amount}}</td></tr>
+        <tr><th>Valor unitario</th><td>{{ $purchase_order->unit_value}}</td></tr>
+        <tr><th>total parcial</th><td>{{ $purchase_order->subtotal}}</td></tr>
+        <tr><th>iva</th><td>{{ $purchase_order->iva}}</td></tr>
+        <tr><th>Retefuente</th><td>{{ $purchase_order->stronghold}}</td></tr>
+        <tr><th>deducciones</th><td>{{ $purchase_order->deductions}}</td></tr>
+        <tr><th>total</th><td>{{ $purchase_order->total}}</td></tr>
+        <tr><th>compra_factura_apoyo</th><td>{{ $purchase_order->ipurchase_invoice_supportd}}</td></tr>
         </table>
         <div class="container">
-            <a href="{{ route('products.index') }}" class="btn">volver</a></h5>
+            <a href="{{ route('purchase_orders.index') }}" class="btn">volver</a></h5>
         </div>
     </div>
 
 @endsection
+
+    
+
+
