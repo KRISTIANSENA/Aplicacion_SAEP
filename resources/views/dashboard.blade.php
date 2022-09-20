caff7c9b244aa17774444971062691668f4cfe2c
riostorresmao <mauricio.rios0@misena.edu.co>
2022-09-19 22:36:43 -0500

    actualizacion menu 2.0

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,27 +6,27 @@
     <title>Menu</title>
     <link href="css/estilos.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
 <div class="estructura">
     <header>
-        <img class="logo" src="img/logotipo.png" width="515" height="70" alt="logotipo">
+        <img class="logo" src="img/saep.jpg" width="1 px" height="10 px" alt="logotipo">
     </header>
     <div class="iconos">
-        <a href="https://www.youtube.com/channel/UCc6SLxTGQaRAWa-ImtDoPPg" target="_blank"><img src="img/video.png" alt="video"><p>SAEP GRUP 2022</p></a>
-        <a href="https://outlook.live.com/owa/" target="_blank"><img src="img/bodega.gif" alt="Bodega" width="80 px"><p>Outlook</p></a>
-        <a href="https://web.whatsapp.com/" target="_blank"><img src="img/ventas.gif" alt="Ventas" width="130 px"><p>Whatsapp</p></a>
-        <a href="https://www.sat.gob.mx/home" target="_blank"><img src="img/inventario.gif" alt="Almacen" width="130 px"><p>SAT</p></a>
-        <a href="https://portalcfdi.facturaelectronica.sat.gob.mx/" target="_blank"><img src="img/administrador.gif" alt="administrador" width="130 px"><p>Facturación SAT</p></a>
-        <a href="https://www.sat.gob.mx/aplicacion/login/43824/reimprime-tus-acuses-del-rfc" target="_blank"><img src="img/rfc.png" alt="rfc"><p>RFC</p></a>
-        <a href="https://tramitesdigitales.sat.gob.mx/InformativaDeTerceros.Internet" target="_blank"><img src="img/diot.png" alt="diot"><p>DIOT</p></a>
-        <a href="https://www.sat.gob.mx/consultas/login/20777/consulta-tu-opinion-de-cumplimiento-de-obligaciones-fiscales" target="_blank"><img src="img/opinion.png" alt="opinion"><p>Opinión de Cumplimiento</p></a>
-        <a href="https://ptscdecprov.clouda.sat.gob.mx/" target="_blank"><img src="img/mensuales.png" alt="mensuales"><p>Declaraciones Mensuales</p></a>
-        <a href="https://rfs.siat.sat.gob.mx/PTSC/RFS/menu/" target="_blank"><img src="img/RIF.png" alt="rif"><p>Régimen de Incorporación Fiscal</p></a>
+
+        <a href="https://outlook.live.com/owa/" target="_blank"><img src="img/bodega.gif" alt="Bodega" width="110 px"><p>Rutas</p></a>
+        <a href="https://web.whatsapp.com/" target="_blank"><img src="img/ventas.gif" alt="Ventas" width="140 px"><p>Ventas</p></a>
+        <a href="https://www.sat.gob.mx/home" target="_blank"><img src="img/inventario.gif" alt="Almacen" width="140 px"><p>Almacen</p></a>
+        <a href="https://portalcfdi.facturaelectronica.sat.gob.mx/" target="_blank"><img src="img/administrador.gif" alt="administrador" width="120 px"><p>Administrador</p></a>
+        <a href="https://www.sat.gob.mx/aplicacion/login/43824/reimprime-tus-acuses-del-rfc" target="_blank"><img src="img/clientes.gif" alt="Clientes" width="90 px"><p>clientes</p></a>
+        <a href="https://tramitesdigitales.sat.gob.mx/InformativaDeTerceros.Internet" target="_blank"><img src="img/proveedores.gif" alt="diot" width="130 px"><p>Proveedores</p></a>
+        <a href="" target="_blank"><img src="img/saep.jpg" alt="video" width=" 100 px"><p>SAEP GRUP 2022</p></a>
+        <a href="https://www.sat.gob.mx/consultas/login/20777/consulta-tu-opinion-de-cumplimiento-de-obligaciones-fiscales" target="_blank"><img src="img/whatsapp.png" alt="opinion"><p>Whatsapp</p></a>
     </div>
-  @include('layouts.components.footer')
+    @include('layouts.components.footer')
 </div>
+
 
 
 </body>
 </html>
+
