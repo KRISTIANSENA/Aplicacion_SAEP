4ac819ce8062f408aad1cbb8d52c715b2d44ba5d
riostorresmao <mauricio.rios0@misena.edu.co>
2022-09-19 23:09:54 -0500

    fonts.1.2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,13 +13,13 @@
     </header>
     <div class="iconos">
 
-        <a href="https://outlook.live.com/owa/" target="_blank"><img src="img/bodega.gif" alt="Bodega" width="110 px"><p>Rutas</p></a>
-        <a href="https://web.whatsapp.com/" target="_blank"><img src="img/ventas.gif" alt="Ventas" width="140 px"><p>Ventas</p></a>
-        <a href="https://www.sat.gob.mx/home" target="_blank"><img src="img/inventario.gif" alt="Almacen" width="140 px"><p>Almacen</p></a>
-        <a href="https://portalcfdi.facturaelectronica.sat.gob.mx/" target="_blank"><img src="img/administrador.gif" alt="administrador" width="120 px"><p>Administrador</p></a>
-        <a href="https://www.sat.gob.mx/aplicacion/login/43824/reimprime-tus-acuses-del-rfc" target="_blank"><img src="img/clientes.gif" alt="Clientes" width="90 px"><p>clientes</p></a>
-        <a href="https://tramitesdigitales.sat.gob.mx/InformativaDeTerceros.Internet" target="_blank"><img src="img/proveedores.gif" alt="diot" width="130 px"><p>Proveedores</p></a>
-        <a href="https://www.sat.gob.mx/consultas/login/20777/consulta-tu-opinion-de-cumplimiento-de-obligaciones-fiscales" target="_blank"><img src="img/whatsapp.png" alt="opinion"><p>Whatsapp</p></a>
+        <a href="https://outlook.live.com/owa/" target="_blank"><img src="img/bodega.gif" alt="Bodega" width="110 px"><p><h2>Rutas</h2></p></a>
+        <a href="https://web.whatsapp.com/" target="_blank"><img src="img/ventas.gif" alt="Ventas" width="140 px"><p><h2>Ventas</h2></p></a>
+        <a href="https://www.sat.gob.mx/home" target="_blank"><img src="img/inventario.gif" alt="Almacen" width="140 px"><p><h2>Almacen</h2></p></a>
+        <a href="https://portalcfdi.facturaelectronica.sat.gob.mx/" target="_blank"><img src="img/administrador.gif" alt="administrador" width="120 px"><p><h2>Administrador</h2></p></a>
+        <a href="https://www.sat.gob.mx/aplicacion/login/43824/reimprime-tus-acuses-del-rfc" target="_blank"><img src="img/clientes.gif" alt="Clientes" width="90 px"><p><h2>clientes</h2></p></a>
+        <a href="https://tramitesdigitales.sat.gob.mx/InformativaDeTerceros.Internet" target="_blank"><img src="img/proveedores.gif" alt="diot" width="130 px"><p><h2>Proveedores</h2></p></a>
+        <a href="https://www.sat.gob.mx/consultas/login/20777/consulta-tu-opinion-de-cumplimiento-de-obligaciones-fiscales" target="_blank"><img src="img/whatsapp.png" alt="opinion"><p><h2>Whatsapp</h2></p></a>
     </div>
     @include('layouts.components.footer')
 </div>
