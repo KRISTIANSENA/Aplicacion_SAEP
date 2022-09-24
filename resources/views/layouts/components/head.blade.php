f57c5d15ceb2686edb08b04bae1ec37f8c5b427c
KRISTIANSENA <90565251+KRISTIANSENA@users.noreply.github.com>
2022-09-24 00:16:07 -0500

    prueba de scrips
    prueba de scrips

--- resources/views/layouts/components/head.blade.php
+++ resources/views/layouts/components/head.blade.php
@@ -12,6 +12,7 @@
     <title>@yield('title')</title>
 
     <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
@@ -19,5 +20,6 @@
 
     <!-- Custom styles for this template-->
     <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
